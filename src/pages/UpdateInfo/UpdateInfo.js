ccd7717e1d5d21f2ea091d49cd5133ac0aa5c333
Your Name <you@example.com>
2022-05-11 20:30:17 +0600

    updated quantity

--- src/pages/UpdateInfo/UpdateInfo.js
+++ src/pages/UpdateInfo/UpdateInfo.js
@@ -1,36 +1,43 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import useReservations from '../../hooks/useReservations';
 
 
 const UpdateInfo = () => {
         const {updateId} = useParams();
+       
+
         
-        const allInventories = useReservations(`https://aqueous-plains-79132.herokuapp.com/inventoryItems?id=${updateId}`)
+        const updateInfo = useReservations(`https://aqueous-plains-79132.herokuapp.com/inventoryItems/${updateId}`)
+        console.log(updateInfo[0])
+    
+        const [reload, setReload] = useState(true)
+       
+        
+        const handleDelivered = () => {
+             const quantity = updateInfo[0].quantity;
+             console.log(quantity)
+             const updateQuantity = quantity - 1;
+             const newQuantity = {quantity: updateQuantity};
+             console.log(newQuantity);
 
-       console.log(allInventories)
-        /* const handleDelivered = () => {
-            const updateQuantity = info.quantity - 1;
-            const newQuantity = {quantity: updateQuantity, ...info.quantity};
-            console.log(newQuantity);
 
-            
-          const url = `https://aqueous-plains-79132.herokuapp.com/${id}`
-           fetch(url, {
-               method: 'PUT',
-               headers: {
-                   'content-type': 'application/json'
-               },
-               body:JSON.stringify(newQuantity),
-           })
-           .then(res=>res.json())
-           .then(data=>{
-               console.log('data are here',data);
-              // alert('quantity-upload')
-           }) 
+            const url = `https://aqueous-plains-79132.herokuapp.com/inventoryItems/${updateId}`
+            fetch(url, {
+                method: 'PUT',
+                headers: {
+                    'content-type': 'application/json'
+                },
+                body:JSON.stringify(newQuantity),
+            })
+            .then(res=>res.json())
+            .then(data=>{
+                console.log('data are here',data);
+               // alert('quantity-upload')
+            })
+            setReload(newQuantity) 
         }
- */
-        const restockRef = useRef();
+ 
         const handleRestock = (e) =>{
             e.preventDefault();
 
@@ -41,14 +48,14 @@ const UpdateInfo = () => {
     return (
         <div className="d-flex justify-content-center mt-3 bg-info">
              <div class="card" style={{width: "25rem"}}>
-               <img style={{height: "20rem"}}  class="card-img-top" alt="..."/>
+               <img style={{height: "20rem"}} src={updateInfo[0].picture} class="card-img-top" alt="..."/>
                <div class="card-body">
-                 <h5 class="card-title">{}</h5>
-                 <p>In Stock: {}</p>
-                 <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <button class="btn btn-primary">Delivered </button> 
+                 <h5 class="card-title">{updateInfo[0].book_name}</h5>
+                 <p>In Stock: {reload}</p>
+                 <p class="card-text">{updateInfo[0].description}</p>
+                    <button onClick={handleDelivered} class="btn btn-primary">Delivered </button> 
                     <form onSubmit={handleRestock}> 
-                        <input ref={restockRef} type="number" name="restock" placeholder="number" id="" />
+                        <input type="number" name="restock" placeholder="number" id="" />
                         <input type="button" value="Restock" />
                     </form>
                </div>
